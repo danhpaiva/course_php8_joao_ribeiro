610311802cdfba72a805a9c4364c326f707394a2
Paiva <danpaiva@live.com>
2021-06-20 22:50:33 -0300

    Using variables

---
+++ class/class05/README.md
@@ -0,0 +1,3 @@
+<h1>Class 04</h1>
+
+<p>Comments in php.</p>
---
+++ class/class05/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$number_five;
+
+$number_five = 5;
+
+echo "Using number: " . $number_five . "<br>";
+
+$number_five = "five";
+
+echo "Using string: " . $number_five . "<br>";
+
+$number_Five = 5;
+
+echo "Using another variable: " . $number_Five . "<br>";

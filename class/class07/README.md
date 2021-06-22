c46e13f3380bcf2c2843f0087caaec4df62bcf9f
Paiva <danpaiva@live.com>
2021-06-21 22:55:31 -0300

    Assignment Operator and Mathematical Operators

---
+++ class/class07/README.md
@@ -0,0 +1,3 @@
+<h1>Class 07</h1>
+
+<p>Assignment Operator and Mathematical Operators in .php</p>
---
+++ class/class07/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$number = 0; //Assignment
+$result = 2 + 2; //Sum
+$result = 2 - 2; //Subtraction 
+$result = 2 * 2; //Multiplication
+$result = 2 / 2; //Division
+$result = 2 % 2; //Module
+
+echo 49 / 7;
+echo '<br>';
+echo 49 % 7;
+
+$a = 10;
+$b = 20;
+
+echo $a + $b;
+echo '<br>';
+echo 4 ** 2; //16

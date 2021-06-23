6342850976dbc0effc8ee8c4bd76beda60516368
Paiva <danpaiva@live.com>
2021-06-22 21:56:54 -0300

    COMBINED OPERATORS | INCREASE AND DECREASE OPERATORS

---
+++ class/class08/README.md
@@ -0,0 +1,3 @@
+<h1>Class 08</h1>
+
+<p>Assignment Operator and Mathematical Operators in .php</p>
---
+++ class/class08/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$number = 10;
+
+echo $number;
+echo "<br>";
+
+echo $number += 10;
+echo "<br>";
+
+$number++;
+echo $number;
+echo "<br>";
+
+$number--;
+echo $number;
+echo "<br>";
+
+--$number;
+echo $number;

39370f611fddda6ae1fd237e030380c97fadd9c1
Paiva <danpaiva@live.com>
2021-07-06 21:47:59 -0300

    'Combined Operators' & Increment and Decrement Operators

---
+++ class/class09/README.md
@@ -0,0 +1,3 @@
+<h1>Class 09</h1>
+
+<p>'Combined Operators' & Increment and Decrement Operators in .php</p>
---
+++ class/class09/index.php
@@ -0,0 +1,18 @@
+<?php
+$number = 10;
+
+$number += 10;
+
+echo $number;
+echo '<br>';
+
+echo $number -= 3;
+echo '<br>';
+
+echo $number++;
+echo '<br>';
+
+echo $number;
+echo '<br>';
+
+echo ++$number;
